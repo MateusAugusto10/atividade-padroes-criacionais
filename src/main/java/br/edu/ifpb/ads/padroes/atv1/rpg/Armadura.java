bc11fb2892d080529af13254b15af9497e4e3346
Mateus <augustomateus643@gmail.com>
2026-06-15 21:42:21 -0300

    implementando Cloneable para cópias independentes de armadura

--- src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/Armadura.java
+++ src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/Armadura.java
@@ -1,27 +1,36 @@
-package br.edu.ifpb.ads.padroes.atv1.rpg;
+package model;
 
-public class Armadura {
+/**
+ * PROTOTYPE: implementa Cloneable para cópias independentes de armaduras.
+ */
+public class Armadura implements Cloneable {
 
     private String nome;
-    private int defesa;
+    private int    defesa;
     private String tipo;
 
     public Armadura(String nome, int defesa, String tipo) {
-        this.nome = nome;
+        this.nome   = nome;
         this.defesa = defesa;
-        this.tipo = tipo;
+        this.tipo   = tipo;
     }
 
-    public String getNome() {
-        return nome;
+    /** Prototype – devolve uma cópia independente desta armadura. */
+    @Override
+    public Armadura clone() {
+        try {
+            return (Armadura) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("Clone não suportado", e);
+        }
     }
 
-    public int getDefesa() {
-        return defesa;
-    }
+    public String getNome()   { return nome;   }
+    public int    getDefesa() { return defesa; }
+    public String getTipo()   { return tipo;   }
 
-    public String getTipo() {
-        return tipo;
+    @Override
+    public String toString() {
+        return String.format("%s (defesa:%d, tipo:%s)", nome, defesa, tipo);
     }
-
 }
