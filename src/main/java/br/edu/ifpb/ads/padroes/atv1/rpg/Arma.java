53a7fe829381feef97f95143d17d3865eea58a82
Mateus <augustomateus643@gmail.com>
2026-06-15 21:37:56 -0300

    implementando Cloneable para permitir cópias de armas

--- src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/Arma.java
+++ src/main/java/br/edu/ifpb/ads/padroes/atv1/rpg/Arma.java
@@ -1,9 +1,13 @@
-package br.edu.ifpb.ads.padroes.atv1.rpg;
+package model;
 
-public class Arma {
+/**
+ * PROTOTYPE: implementa Cloneable para permitir cópias de armas
+ * sem acoplar o cliente à classe concreta.
+ */
+public class Arma implements Cloneable {
 
     private String nome;
-    private int dano;
+    private int    dano;
     private String tipo;
 
     public Arma(String nome, int dano, String tipo) {
@@ -12,15 +16,22 @@ public class Arma {
         this.tipo = tipo;
     }
 
-    public String getNome() {
-        return nome;
+    /** Prototype – devolve uma cópia independente desta arma. */
+    @Override
+    public Arma clone() {
+        try {
+            return (Arma) super.clone(); // campos são primitivos/String: cópia rasa é suficiente
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("Clone não suportado", e);
+        }
     }
 
-    public int getDano() {
-        return dano;
-    }
+    public String getNome() { return nome; }
+    public int    getDano() { return dano; }
+    public String getTipo() { return tipo; }
 
-    public String getTipo() {
-        return tipo;
+    @Override
+    public String toString() {
+        return String.format("%s (dano:%d, tipo:%s)", nome, dano, tipo);
     }
 }
